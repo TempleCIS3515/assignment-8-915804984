3d7951dd7015f3306680bdbaeda8af5b9398021b
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-11-09 22:03:19 -0500

    commit 3: updated Manifest and MainActivity

--- app/src/main/java/edu/temple/flossplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/flossplayer/MainActivity.kt
@@ -4,9 +4,22 @@ import android.app.SearchManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
 
 class MainActivity : AppCompatActivity() {
+
+    var url = "https://kamorris.com/lab/flossplayer/search.php?query=search_term" //correct URL?
+
+    //private val JARGON: String = ""
+
+    //for search button
+    private var buttonSearch: Button = findViewById(R.id.SearchButton)
 
     private val isSingleContainer : Boolean by lazy{
         findViewById<View>(R.id.container2) == null
@@ -16,19 +29,34 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this)[BookViewModel::class.java]
     }
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bookViewModel.setBookList(getBookList())
 
-        //receiving the query
+        val queue: RequestQueue = Volley.newRequestQueue(applicationContext) //need?
+
+        //when search button is clicked...
+        buttonSearch.setOnClickListener {
+            fun onClick() {
+                onSearchRequested()
+            }
+        }
+
+        //receiving the query...check
         if (Intent.ACTION_SEARCH == intent.action)
         {
             intent.getStringExtra(SearchManager.QUERY)?.also{ query ->
                 doMySearch(query)
             }
         }
+
+        //type-to-search functionality
+        setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL)
 
         // If we're switching from one container to two containers
         // clear BookPlayerFragment from container1
@@ -72,6 +100,16 @@ class MainActivity : AppCompatActivity() {
                 bookViewModel.markSelectedBookViewed()
             }
         }
+    }
+
+    //check with this
+    //val jargon: Boolean = intent.getBundleExtra(SearchManager.APP_DATA)?.getBoolean(JARGON) ?: false
+    override fun onSearchRequested(): Boolean {
+        val appData = Bundle().apply {
+            //putBoolean(JARGON, true)
+        }
+        startSearch(null, false, appData, false)
+        return true
     }
 
     override fun onBackPressed() {
